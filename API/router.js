57590c712c082434a01136cdc03905a8ed7b56ff
Ancient Abysswalker <ancientabysswalker@gmail.com>
2020-09-13 15:12:30 -0700

    >Implemented POST >Implemented GET >Implemented GET>id >Pretty-ifying console logging

--- API/router.js
+++ API/router.js
@@ -5,48 +5,93 @@ const app = express();
 const config = require("./config.js");
 
 // Retrieve Mongo Client
-const MongoClient = require("mongodb").MongoClient;
+const mongo = require("mongodb");
+const MongoClient = mongo.MongoClient;
 
 // Use body-parser to parse the body af a request
 let bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
+// Log that process is starting
+console.log(c_cyn("Attempting to start mongo router"));
+
+// Parse args for process
+let p_args = process.argv.slice(2);
+let log_file = !p_args.includes("-nolog");
+
 // Connect to the db and listen
 MongoClient.connect(
   config.uri,
   { useNewUrlParser: true, useUnifiedTopology: true },
-  (err, client) => {
-    if (!err) {
+  (client_err, client) => {
+    if (!client_err) {
       let db = client.db(config.db);
-      let recipes = db.collection("stars");
+      let recipes = db.collection("recipes");
 
       // [POST] a new recipe to the mongo db
-      app.post("/stars", (req, res) => {
+      app.post("/recipes", (request, response) => {
         try {
-          if (req.body.name === undefined || req.body.distance === undefined)
-            throw "nonull";
+          // if (req.body.name === undefined || req.body.distance === undefined)
+          //   throw "nonull";
           let new_entry = {
-            name: req.body.name,
-            distance: req.body.distance,
+            name: request.body.name,
+            time_active: request.body.time_active,
+            time_passive: request.body.time_passive,
+            ingredients: request.body.ingredients,
+            instructions: request.body.instructions,
           };
 
-          recipes.insertOne(new_entry, (op_err, op_res) => {
-            res.send(op_res.ops[0]);
+          // Insert one recipe
+          recipes.insertOne(new_entry, (err, result) => {
+            if (!err) {
+              response.status(201).send(result.ops[0]);
+            } else {
+              response.status(500).send({
+                message: "Internal database exception",
+              });
+            }
           });
         } catch (err) {
-          res.status(400).send({
+          // Switch here for errors
+          response.status(400).send({
             message: "Incorrect or insufficient data in [POST] body",
           });
         }
       });
 
       // [GET] the full body data for all recipes - Not recommended
-      app.get("/stars", (req, res) => {
-        recipes.find().toArray((err, items) => {
+      app.get("/recipes", (request, response) => {
+        recipes.find().toArray((err, result) => {
           if (!err) {
-            res.json(items);
+            response.json(result);
           } else {
-            console.log("error", err);
+            console.log("Query Error:", err);
+            response.status(500).send({
+              message: "Internal database exception",
+            });
+          }
+        });
+      });
+
+      // [GET] the full body data for one recipe using its db id
+      app.get("/recipes/:id", (request, response) => {
+        console.log(
+          `Recieved ${c_yel("[GET]")} request for id ${request.params.id}`
+        );
+
+        //trycatch HERE - see error from too-long id field!
+        // Get the id in an acceptable format for mongo
+        let id = new mongo.ObjectID(request.params.id);
+
+        recipes.findOne({ _id: id }, (err, result) => {
+          if (!err) {
+            console.log(`${c_yel("[GET]")} request ${c_grn("successful")}\n`);
+            response.json(result);
+          } else {
+            console.log(c_red("Query Error:"), err);
+            response.status(500).send({
+              message: "Internal database exception",
+            });
           }
         });
       });
@@ -81,10 +126,29 @@ MongoClient.connect(
       });
 
       app.listen(3000, function() {
-        console.log("starting our application");
+        console.log(c_grn("Mongo router successfully started on port 3000"));
+        console.log(
+          ` - Logging to file is currently ${
+            log_file ? c_grn("enabled") : c_red("disabled")
+          }\n`
+        );
       });
     } else {
-      console.log("error", err);
+      console.log("Mongo router failed to start:", client_err);
     }
   }
 );
+
+// Color interrupt functions for in-console strings
+function c_yel(str) {
+  return `\x1b[33m${str}\x1b[0m`;
+}
+function c_grn(str) {
+  return `\x1b[32m${str}\x1b[0m`;
+}
+function c_cyn(str) {
+  return `\x1b[36m${str}\x1b[0m`;
+}
+function c_red(str) {
+  return `\x1b[31m${str}\x1b[0m`;
+}
