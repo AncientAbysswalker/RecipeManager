631c44441955a35fb355f70890212ce9811030aa
Ancient Abysswalker <ancientabysswalker@gmail.com>
2020-09-15 08:15:54 -0700

    Upgrading router and adding status codes

--- API/router.js
+++ API/router.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-control-regex */
 const express = require("express");
 const app = express();
+const fs = require("fs");
 
 // Load config
 const config = require("./config.js");
@@ -31,8 +33,7 @@ MongoClient.connect(
       // [POST] a new recipe to the mongo db
       app.post("/recipes", (request, response) => {
         try {
-          // if (req.body.name === undefined || req.body.distance === undefined)
-          //   throw "nonull";
+          // Build new entry from request body. Any fields not included will become null
           let new_entry = {
             name: request.body.name,
             time_active: request.body.time_active,
@@ -59,72 +60,95 @@ MongoClient.connect(
         }
       });
 
-      // [GET] the full body data for all recipes - Not recommended
+      // [GET] the data for all recipes, with optional filtering
       app.get("/recipes", (request, response) => {
-        recipes.find().toArray((err, result) => {
-          if (!err) {
-            response.json(result);
-          } else {
-            console.log("Query Error:", err);
-            response.status(500).send({
-              message: "Internal database exception",
+        try {
+          // Ensure that the fields will always be a list
+          let fields = paramToList(request.query.fields);
+
+          // Query mongo db
+          recipes
+            .find()
+            .project(fields.reduce((a, b) => ((a[b] = 1), a), {}))
+            .toArray((err, result) => {
+              if (!err) {
+                response.json(result);
+              } else {
+                console.log("Query Error:", err);
+                response.status(500).send({
+                  message: "Internal database exception",
+                });
+              }
             });
-          }
-        });
+        } catch (err) {
+          // Switch here for errors
+          response.status(400).send({
+            message: "Incorrect or insufficient data in [POST] body",
+          });
+        }
       });
 
-      // [GET] the full body data for one recipe using its db id
+      // [GET] the data for one recipe using its db id, with optional filtering
       app.get("/recipes/:id", (request, response) => {
         console.log(
           `Recieved ${c_yel("[GET]")} request for id ${request.params.id}`
         );
 
-        //trycatch HERE - see error from too-long id field!
-        // Get the id in an acceptable format for mongo
-        let id = new mongo.ObjectID(request.params.id);
+        // First confirm that the id request is OK
+        try {
+          let id = new mongo.ObjectID(request.params.id);
 
-        recipes.findOne({ _id: id }, (err, result) => {
-          if (!err) {
-            console.log(`${c_yel("[GET]")} request ${c_grn("successful")}\n`);
-            response.json(result);
-          } else {
-            console.log(c_red("Query Error:"), err);
-            response.status(500).send({
-              message: "Internal database exception",
+          try {
+            recipes.findOne({ _id: id }, (err, result) => {
+              if (!err) {
+                console.log(
+                  `${c_yel("[GET]")} request ${c_grn("successful")}\n`
+                );
+                fs.appendFileSync(
+                  "./log.txt",
+                  stripColor(
+                    `${c_yel("[GET]")} request ${c_grn("successful")}\n`
+                  )
+                );
+
+                // If response is null, respond 404
+                if (result !== null) {
+                  response.json(result);
+                } else {
+                  response.status(404).send({
+                    status: 404,
+                    message: "The requested resource could not be found",
+                  });
+                }
+              } else {
+                console.log(c_red("Query Error:"), err);
+                response.status(500).send({
+                  message: "Internal database exception",
+                });
+              }
+            });
+          } catch (err) {
+            console.log(err.name);
+            console.log(err.message);
+            console.log(err.stack);
+            // Switch here for errors
+            response.status(400).send({
+              message: "Incorrect or insufficient data in [POST] body",
             });
           }
-        });
-      });
-
-      //get a specific post by URL, the url is a unique index in mongodb
-      app.get("/star/:name", function(req, res) {
-        var name = req.params.name;
-        console.log(name);
-        if (typeof name == "string") {
-          recipes.findOne({ name: name }, function(err, item) {
-            if (!err) {
-              res.json(item);
-            } else {
-              console.log("error", err);
-            }
+        } catch {
+          console.log(
+            "The id provided must be a single string of 12 bytes or 24 hex characters"
+          );
+          response.status(400).send({
+            status: 400,
+            message:
+              "The id provided must be a single string of 12 bytes or 24 hex characters",
           });
         }
       });
 
-      //get the posts by category
-      app.get("/posts/category/:cat", function(req, res) {
-        var cat = req.params.cat;
-        if (typeof cat == "string") {
-          recipes.find({ category: cat }).toArray(function(err, item) {
-            if (!err) {
-              res.json(item);
-            } else {
-              console.log("error", err);
-            }
-          });
-        }
-      });
-
+      // Indicate successful start in the console
       app.listen(3000, function() {
         console.log(c_grn("Mongo router successfully started on port 3000"));
         console.log(
@@ -134,7 +158,7 @@ MongoClient.connect(
         );
       });
     } else {
-      console.log("Mongo router failed to start:", client_err);
+      console.log(c_red("Mongo router failed to start:"), client_err);
     }
   }
 );
@@ -151,4 +175,18 @@ function c_cyn(str) {
 }
 function c_red(str) {
   return `\x1b[31m${str}\x1b[0m`;
+}
+function stripColor(str) {
+  return str.replace(/\x1b\[[0-9]+m/g, "");
+}
+
+function paramToList(param) {
+  switch (typeof param) {
+    case "string":
+      return [param];
+    case "object":
+      return param;
+    default:
+      return [];
+  }
 }
