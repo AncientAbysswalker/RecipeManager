2b7fca38fa03ada8cd43b0640c513c5e8f14d5d2
Ancient Abysswalker <ancientabysswalker@gmail.com>
2020-10-05 20:53:49 -0700

    Adding RecipePage

---
+++ components/InstructionSection.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ this.section.title }}</h1>
+    <p v-if="section.hasOwnProperty('steps')">reddit</p>
+    <div
+      v-else
+      v-for="subsection in this.section.subsections"
+      :key="subsection.title"
+    >
+      <InstructionSection :section="subsection" />
+    </div>
+  </div>
+</template>
+
+ 
+
+<script>
+export default {
+  name: "InstructionSection",
+  props: ["section"],
+};
+</script>
